bdd3d8b3199051c7cf1464386217414be661b6f7
Hryhorii Kyrylchenko <gregorykirilchenko@gmail.com>
2024-04-28 15:58:00 +0200

    Add project files.

---
+++ OnlineStoreProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OnlineStoreProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
