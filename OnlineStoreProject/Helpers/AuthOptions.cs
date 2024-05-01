458dce1f137d4e400c885b3c7f916176678104cf
Hryhorii Kyrylchenko <gregorykirilchenko@gmail.com>
2024-05-01 18:48:40 +0200

    Added header and footer to the layout code. Minor edits have been made.

---
+++ OnlineStoreProject/Helpers/AuthOptions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace OnlineStoreProject.Helpers
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "OnlineShopAuthServer";
+        public const string AUDIENCE = "OnlineShopUser";
+        public const string KEY = "MySuperSecret_key#forOnlineShop!123#@#@#@#";
+        public static SymmetricSecurityKey GetSymmetricSecurityKey() =>
+            new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+    }
+}
--- OnlineStoreProject/Program.cs
+++ OnlineStoreProject/Program.cs
@@ -1,11 +1,42 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OAuth;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using OnlineStoreProject.Helpers;
 using OnlineStoreProject.Models.Database;
+using OnlineStoreProject.Servises;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddAuthentication();
+builder.Services.AddSingleton<AuthOptions>();
+builder.Services.AddScoped<JwtTokenService>();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidIssuer = AuthOptions.ISSUER,
+        ValidateAudience = true,
+        ValidAudience = AuthOptions.AUDIENCE,
+        ValidateLifetime = true,
+        IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+        ValidateIssuerSigningKey = true,
+    };
+})
+.AddCookie(options =>
+{
+    options.LoginPath = "/login";
+});
 builder.Services.AddAuthorization();
 
 string? connection = builder.Configuration.GetConnectionString("DefaultConnection");
